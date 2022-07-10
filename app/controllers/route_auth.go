863f19374230c87cdc124a2324689c598614285d
karasawa <pain.keita.27@gmail.com>
2022-07-11 07:29:28 +0900

    add login

---
+++ app/controllers/route_auth.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"log"
+	"main/app/models"
+	"net/http"
+)
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		genelateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name: r.PostFormValue("name"),
+			Email: r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	genelateHTML(w, nil, "layout", "public_navbar", "login")
+}
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"main/app/controllers"
+	"main/app/models"
 )
 
 func main() {
 	controllers.StartMainServer()
+
+	user, _ := models.GetUserByEmail("test@gmail.com")
+	fmt.Println(user)
+
 }
